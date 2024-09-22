e221e6ce9c87d95858d96cf65837f7d25cb3f7c2
Maria lina <140550353+Marialinaa@users.noreply.github.com>
2024-09-22 18:20:55 -0300

    Add files via upload

---
+++ orde.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+ using namespace std;
+ void metade_crescente(int vet[],int tam){
+	 
+	 
+	 
+	 }
+void metade_Decresce(int vet[],int tam){
+	int valor,i=0;
+	int meio;
+	meio=vet[i]/vet[i];
+       for(int j=0 ;j<tam;j++){
+		   valor=vet[j];
+		   }	
+		   while((i>0)and (vet[i-1]<valor)){
+			   vet[i]=vet[i+1];
+			   i--;
+			   vet[i]=valor; 
+			   }
+	
+	}
+	
+ int main (){
+	int tam;
+	cin>>tam;
+	int vetor[];
+	if(tam%2==0){
+	int vetor[tam];
+}
+	for(int i=0;i<tam;i++){
+		cin>>vetor[i];
+		}
+		int j,c,f;
+		while(j<tam){
+			
+			
+			}
+
+	 return 0;
+	 }
+	
+
+
+
+
+
